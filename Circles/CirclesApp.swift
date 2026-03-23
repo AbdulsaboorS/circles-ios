dc295632315b3aaa68c54741cb1f37971f3e3538
abdulaboor <shaikhabdulsaboor1@gmail.com>
2026-03-23 01:15:36 -0500

    Initial Commit

---
+++ Circles/CirclesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CirclesApp.swift
+//  Circles
+//
+//  Created by Abdulsaboor Shaikh on 3/23/26.
+//
+
+import SwiftUI
+
+@main
+struct CirclesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
